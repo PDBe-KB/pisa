e9491f348d50837e7dd6099fd50dc39ac62904d4
Grisell <grisell83@gmail.com>
2025-11-14 14:36:14 +0000

    Improving compile.sh:added error if SRCDIR not set, warning supression, no fail in build directory exists

--- compile.sh
+++ compile.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+set -e
 
-# Modify srcdir below to point to complete path of the PISA-LITE directory
-# For example: srcdir=/Users/foo/bar/pisa-lite
+# Modify srcdir below to point to complete path of the PISA directory                                                                              
+# For example: srcdir=/Users/foo/bar/pisa
+# Ensure SRCDIR is set
+if [ -z "$SRCDIR" ]; then
+    echo "Error: SRCDIR is not set"
+    exit 1
+fi
 
-srcdir=$SRCDIR
+srcdir="$SRCDIR"
 
-mkdir build
+builddir="$srcdir/build"
+ssmdir="$srcdir/ssm"
+pisalibdir="$srcdir/pisalib"
+mmdbdir="$srcdir/mmdb2"
+srsdir="$srcdir/ccp4srs"
+pisadir="$srcdir/pisa"
 
-builddir=$srcdir/build
-ssmdir=$srcdir/ssm
-pisalibdir=$srcdir/pisalib
-mmdbdir=$srcdir/mmdb2
-srsdir=$srcdir/ccp4srs
-pisadir=$srcdir/pisa
+mkdir -p "$builddir"
+cd "$builddir"
 
-cd $builddir
+# compile each component
+g++ -c -I"$srcdir" "$mmdbdir"/*.cpp -Wno-deprecated-declarations
+g++ -c -I"$srcdir" "$ssmdir"/*.cpp -Wno-deprecated-declarations
+g++ -c -I"$srcdir" "$srsdir"/*.cpp -Wno-deprecated-declarations
+g++ -c -I"$srcdir" "$pisalibdir"/*.cpp -Wno-deprecated-declarations
+g++ -c -I"$srcdir" "$pisadir"/*.cpp -Wno-deprecated-declarations
 
-g++ -c -I$srcdir $mmdbdir/*.cpp
-g++ -c -I$srcdir $ssmdir/*.cpp
-g++ -c -I$srcdir $srsdir/*.cpp
-g++ -c -I$srcdir $pisalibdir/*.cpp
-g++ -I$srcdir $pisadir/*.cpp *.o -lz -o pisa
+# link all objects
+g++ *.o -lz -o pisa
